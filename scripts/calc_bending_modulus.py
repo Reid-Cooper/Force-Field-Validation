1eb5f197ef11002e90f954b21383c9ed9d83bed2
Reid <reidxcooper@gmail.com>
2026-06-16 15:20:04 -0600

    Update temperature and enhance output results
    Updated temperature variable and modified output format to include additional details about bilayer thickness, lipids per leaflet, and force field type.

--- scripts/calc_bending_modulus.py
+++ scripts/calc_bending_modulus.py
@@ -10,7 +10,7 @@ if len(sys.argv) < 4:
 
 # set up variables and constants
 kB = 1.380649e-23 # Boltzmann constant in J/K
-T = 300 # Temperature in K
+T = 303 # Temperature in K
 N = int(sys.argv[2]) # lipids per leaflet
 bilayerThickness = float(sys.argv[3]) # bilayer thickness, h, in nm
 A2toM2 = 1e-20 # conversion factor from Å^2 to m^2
@@ -26,14 +26,27 @@ var_A = statistics.pvariance(areas)
 compressibilityModulus = (kB * T * mean_A) / var_A #KA, stiffness of the membrane (N/m), (KB * T * <A>) / Var(A)
 bendingModulus = (compressibilityModulus * (bilayerThickness * 1e-9)**2) / 48 #KB also known as Kappa or bending modulus (J), (KA * h^2) / 48
 
-# print results
-print(f"-----------------------------------------")
-print(f"<A> = {mean_A:.3e} m²")
-print(f"Var(A) = {var_A:.3e} m⁴")
-print(f"-----------------------------------------")
-print(f"K_A = {compressibilityModulus:.3f} N/m")
-print(f"K_A = {compressibilityModulus*1000:.1f} mN/m")
-print(f"-----------------------------------------")
-print(f"K_B = {bendingModulus:.3e} J")
-print(f"K_B = {bendingModulus/(kB*T):.2f} kBT") 
-print(f"-----------------------------------------")
+# print and save results
+results = f"""
+-----------------------------------------
+Bilayer Thickness = {bilayerThickness} nm
+Lipids Per Leaflet = {N}
+Force Field Type = Sage 2.3.0
+MD Production Runtime = 160 ns
+-----------------------------------------
+<A> = {mean_A:.3e} m²
+Var(A) = {var_A:.3e} m⁴
+-----------------------------------------
+K_A = {compressibilityModulus:.3f} N/m
+K_A = {compressibilityModulus*1000:.1f} mN/m
+-----------------------------------------
+K_C = {bendingModulus:.3e} J
+K_C = {bendingModulus/(kB*T):.2f} kBT
+-----------------------------------------
+"""
+
+print(results)
+
+# saves results to an output file
+with open("metric_results/bending_modulus_Sage230_POPC256_bending_modulus.txt", "w") as f:
+    f.write(results)
